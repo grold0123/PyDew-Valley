0a6c363965c0c2019c8887e2ee6aede499956373
grold0123 <172453552+grold0123@users.noreply.github.com>
2025-11-07 12:35:33 +0800

    update

---
+++ repo.py
@@ -0,0 +1,20 @@
+import subprocess
+
+base_dir = r"C:\projects\PyDew-Valley"
+
+
+args = [
+    ['git','add',base_dir],#git add
+    ['git','commit','-m','update'],#git commit
+    ['git','push'],#git push
+]
+
+for arg in args:
+    print('\n\n')
+    result = subprocess.run(arg)
+    if result.returncode == 0:
+        continue
+    print('\nRepository not updated')
+    break
+
+print('\nRepository updated')
